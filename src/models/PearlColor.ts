2feadf148a9d02327409e7ae046d197f1b8639aa
Stephen Cherry <stephen.m.cherry@outlook.com>
2025-12-13 14:40:20 -0600

    Moved color hex parsing to PearlColor

--- src/models/PearlColor.ts
+++ src/models/PearlColor.ts
@@ -9,4 +9,27 @@ enum PearlColor {
 	Magenta = "magenta"
 }
 
-export { PearlColor };
+const GetPearlHexColor = (color: PearlColor): number => {
+	switch (color) {
+		case PearlColor.White:
+			return 0xffffff;
+		case PearlColor.Black:
+			return 0x000000;
+		case PearlColor.Red:
+			return 0xff0000;
+		case PearlColor.Yellow:
+			return 0xffff00;
+		case PearlColor.Green:
+			return 0x00ff00;
+		case PearlColor.Blue:
+			return 0x0000ff;
+		case PearlColor.Cyan:
+			return 0x00ffff;
+		case PearlColor.Magenta:
+			return 0xff00ff;
+		default:
+			return 0xffffff;
+	}
+}
+
+export { PearlColor, GetPearlHexColor };
